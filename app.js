dc02c2e31ba41edd872296257eb2f30f87465327
Jamienorthman <mikoann88@gmail.com>
2023-11-08 12:19:15 +0100

    function prodcues html for color palettes

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const schemeSelectBox = document.getElementById('choose-scheme')
 const getSchemeBtn = document.querySelector('button')
 const seedColor = document.querySelector('input')
-const schemeOption = document.querySelectorAll('option')
+
+renderColors()
 
 getSchemeBtn.addEventListener('click', () => {
-   
+    renderColors()
+})
 
+function renderColors() {
     fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor.value.replace('#', '')}&mode=${schemeSelectBox.value}`)
         .then(res => res.json())
-        .then(data => console.log(data))
-})
+        .then(data => {
+        let colorHtml = ''
+            for (let color of data.colors) {
+                colorHtml += `
+                <div class="color" style="background-color:${color.hex.value}">
+                    <h3 class="color-code">${color.hex.value}</h3>
+                </div>
+                `
+            }
+            document.querySelector('.color-palette').innerHTML = colorHtml
+        })
+}
+
+    
+    
     
     
 
